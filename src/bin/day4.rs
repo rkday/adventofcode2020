c7bf93550c804abc2cc61559ee9f843b53a9fe04
Rob Day <rkd@rkd.me.uk>
2020-12-05 09:50:03 +0000

    Day4 under100 lines

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -4,61 +4,31 @@ use nom::{
     bytes::streaming::tag,
     character::complete::{char, digit1, hex_digit1},
     sequence::tuple,
-    IResult,
 };
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-fn part1() {
-    let mut passport_lines = Vec::new();
-    let mut s = String::new();
-    for line in std::fs::read_to_string("input_day4.txt").unwrap().lines() {
-        if line == "" {
-            passport_lines.push(s.clone());
-            s.clear();
-        } else {
-            s.push_str(line);
-            s.push_str(" ");
-        }
-    }
-    println!("{:#?}", passport_lines);
-    println!("{:#?}", passport_lines.len());
-    let valid = passport_lines
-        .iter()
-        .filter(|s| s.contains("byr:"))
-        .filter(|s| s.contains("iyr:"))
-        .filter(|s| s.contains("eyr:"))
-        .filter(|s| s.contains("hgt:"))
-        .filter(|s| s.contains("hcl:"))
-        .filter(|s| s.contains("ecl:"))
-        .filter(|s| s.contains("pid:"))
-        .count();
-    println!("{}", valid);
-}
-
-fn get_next_passport<'a, I>(i: &mut I) -> String
+fn get_next_passport<'a, I>(i: &mut I) -> Option<String>
 where
     I: Iterator<Item = &'a str>,
 {
     i.skip_while(|s| s.is_empty())
         .take_while(|s| !s.is_empty())
-        .fold(String::new(), |acc, s| format!("{} {}", acc, s))
+        .fold(None, |acc, s| {
+            Some(format!("{} {}", acc.unwrap_or_else(String::new), s))
+        })
 }
 
 fn valid_hgt(input: &str) -> bool {
-    let result: IResult<_, _> = tuple((digit1, alt((tag("cm"), tag("in")))))(input);
-    if let Ok((_, (num, type_))) = result {
-        if type_ == "in" {
-            (59..=76).contains(&num.parse::<u32>().unwrap_or(0))
-        } else {
-            (150..=193).contains(&num.parse::<u32>().unwrap_or(0))
-        }
-    } else {
-        false
+    let result: nom::IResult<_, _> = tuple((digit1, alt((tag("cm"), tag("in")))))(input);
+    match result {
+        Ok((_, (num, "in"))) => (59..=76).contains(&num.parse::<u32>().unwrap_or(0)),
+        Ok((_, (num, "cm"))) => (150..=193).contains(&num.parse::<u32>().unwrap_or(0)),
+        _ => false,
     }
 }
 
 fn valid_ecl(input: &str) -> bool {
-    let result: IResult<_, _> = alt((
+    let result: nom::IResult<_, _> = alt((
         tag("amb"),
         tag("blu"),
         tag("brn"),
@@ -67,29 +37,17 @@ fn valid_ecl(input: &str) -> bool {
         tag("hzl"),
         tag("oth"),
     ))(input);
-    if let Ok((remaining, _)) = result {
-        remaining == ""
-    } else {
-        false
-    }
+    result.map(|(remaining, _)| remaining) == Ok("")
 }
 
 fn valid_pid(input: &str) -> bool {
-    let result: IResult<_, _> = digit1(input);
-    if let Ok((_, num)) = result {
-        num.len() == 9
-    } else {
-        false
-    }
+    let result: nom::IResult<_, _> = digit1(input);
+    result.map(|(_, num)| num.len()) == Ok(9)
 }
 
 fn valid_hcl(input: &str) -> bool {
-    let result: IResult<_, _> = tuple((char('#'), hex_digit1))(input);
-    if let Ok((_, (_, hex))) = result {
-        hex.len() == 6
-    } else {
-        false
-    }
+    let result: nom::IResult<_, _> = tuple((char('#'), hex_digit1))(input);
+    result.map(|(_, (_, hex))| hex.len()) == Ok(6)
 }
 
 fn valid_passport(m: &HashMap<String, String>) -> Option<bool> {
@@ -104,47 +62,38 @@ fn valid_passport(m: &HashMap<String, String>) -> Option<bool> {
     )
 }
 
-fn part2() {
-    let input = std::fs::read_to_string("input_day4.txt").unwrap();
-    let raw_lines = input.lines().peekable();
+fn valid_passport_part1(m: &HashMap<String, String>) -> bool {
+    let required_keys = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+    m.keys()
+        .map(|s| s.to_owned())
+        .collect::<HashSet<_>>()
+        .is_superset(&required_keys.into_iter().map(|s| s.to_owned()).collect())
+}
 
-    let passports: Vec<HashMap<_, _>> = raw_lines
-        .batching(|i| {
-            if i.peek().is_some() {
-                Some(get_next_passport(i))
-            } else {
-                None
-            }
-        })
+fn main() {
+    let input = std::fs::read_to_string("input_day4.txt").unwrap();
+
+    let passports = input
+        .lines()
+        .batching(|i| get_next_passport(i))
         .map(|passport| {
             passport
                 .split(" ")
                 .filter_map(|s| {
-                    if s.is_empty() {
-                        None
-                    } else {
-                        let mut parts = s.split(':');
-                        Some((
-                            parts.next().unwrap().to_owned(),
-                            parts.next().unwrap().to_owned(),
-                        ))
-                    }
+                    s.split(':')
+                        .next_tuple()
+                        .map(|(a, b)| (a.to_owned(), b.to_owned()))
                 })
-                .collect()
-        })
-        .collect();
+                .collect::<HashMap<_, _>>()
+        });
 
-    println!("{:#?}", passports);
+    let part2_passports = passports.clone();
+
     println!(
-        "{}",
-        passports
-            .iter()
+        "Part 1: {}, part 2: {}",
+        passports.filter(|p| valid_passport_part1(p)).count(),
+        part2_passports
             .filter(|p| valid_passport(p) == Some(true))
             .count()
     );
-}
-
-fn main() {
-    part1();
-    part2();
 }
